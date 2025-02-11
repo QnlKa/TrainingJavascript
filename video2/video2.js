4325fc07fb64c6011080e1b039c4c4264a3933bb
Pham Quan <phamquan@Phams-MacBook-Pro.local>
2025-02-11 19:10:51 +0700

    video5

--- video2/video2.js
+++ video2/video2.js
@@ -1,2 +1,6 @@
 alert('Chào mày!')
 console.log('Learning JavaScript')
+let x = 5, y = 7, z = 9
+console.log('Giá trị x = '+x+'. Giá trị y = '+y+'. Giá trị z = '+z)
+console.log('hihi'+5+3)
+console.log(`${y} handsome guys`)
---
+++ video2/video5.js
@@ -0,0 +1,16 @@
+console.log(`Hello world from HTML!!`)
+//string method#1 .length
+let x = `string 1 haha`
+console.log(x.length)
+
+//string method#2 substr
+let y = `string 2 huhu`
+console.log(y.substr(-6,2))
+
+//string method#3 replace
+let z = `string 3 hihi`
+console.log(z.replace(`it's not string 3`))
+
+//string method#4 split
+let h = `string 4 "hmmm.."`
+console.log(h.split(' '))
